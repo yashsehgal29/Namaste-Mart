c3cf8600573f8822ac63ccc123582d92abb40a72
yashsehgal29 <yashsehgal2974@gmail.com>
2024-06-10 12:00:01 +0530

    edited assets

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Section1 = () => {
   return (
     <div >
-      <img src="/src/assets/Frame 1.png" className='w-full' alt="img" />
+      <img src="https://drive.google.com/file/d/1uXVFLPLUnWpjpy0L7LbLzm4yeA2rOLIU/view?usp=sharing/src/assets/Frame 1.png" className='w-full' alt="img" />
     </div>
   )
 }
